17bea2bf4e35394c19acae5785b97d54bc9a637b
salemferchouche <ferchouchesalem23@gmail.com>
2021-10-27 15:48:24 +0200

    I finalized the user choice loop in the Menu, I also added the emojis

--- src/com/fass/Menu.java
+++ src/com/fass/Menu.java
@@ -2,26 +2,25 @@ package com.fass;
 
 import java.util.Locale;
 import java.util.Scanner;
-
+// creation of the class menu
 //Creation of the game menu
 public class Menu {
     public static void displayMainMenu() {
         System.out.println("Welcome to the Hit The Beaver 🔨💥");
         System.out.println("Make your choice");
-        System.out.println("S-StarGame");
+        System.out.println("S-StartGame");
         System.out.println("V-See scores");
         System.out.println("Q-Quits");
 
         Scanner scan = new Scanner(System.in);
         String selection = scan.nextLine();
         selection = selection.toLowerCase();
-        if (selection.equals("s")) {
-            ChoicePlayer.displayMainMenu();
-        } else if (selection.equals("v")) {
-            ChoicePlayer.displayMainMenu();
-        } else if (selection.equals("q")) {
-            ChoicePlayer.displayMainMenu();
+        switch (selection) {
+            case "s" -> ChoicePlayer.displayMainMenu();
+            case "v" -> ChoicePlayer.displayMainMenu();
+            case "q" -> ChoicePlayer.displayMainMenu();
         }
+
 
     }
 
