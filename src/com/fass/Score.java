3540ead1d65a14a5c65a0fc63e6365669737e543
Sophie <sofiefrance@hotmail.fr>
2021-10-27 11:08:43 +0200

    base score creation

---
+++ src/com/fass/Score.java
@@ -0,0 +1,25 @@
+package com.fass;
+
+public class Score {
+
+    public static void main(String[] args) {
+
+        //entrer les scores
+
+        int nombre_parties_effectuees = 0 ;
+        tmp_score = 0;
+
+
+        int [] tableau_score = new int [nombre_parties_effectees];
+
+        // afficher les 5 meilleurs score dans l'ordre
+
+        for (int i =0; i< nombre_parties_effectuees; i++ ) {
+
+            if (tmp_score
+        }
+
+
+
+    }
+
