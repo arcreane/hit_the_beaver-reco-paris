708e7221c600f1803a3d5f04b1902945edc87b82
AmalGuedda <guedda94320@gmail.com>
2021-10-27 17:14:11 +0200

    I applied a while loop because we just want to accept uniquely "Easy", "Medium" or "Hard

--- src/com/fass/ChoicePlayer.java
+++ src/com/fass/ChoicePlayer.java
@@ -13,17 +13,28 @@ public class ChoicePlayer {
         System.out.println("Please choose the level you want");
         String userinput = input.nextLine();
 
+        // We applied a while loop because we just want to accept uniquely "Easy", "Medium" or "Hard
+        while (!userinput.equals("Easy") && !userinput.equals("Medium") && !userinput.equals("Hard")){
+            System.out.println("Please, enter your choice again");
+            userinput = input.nextLine();
+        }
+
+        // We used if because we want to have the choice of the player between "Easy", "Medium" or "Hard"
         if (userinput.equals(easy)) {
             System.out.println("You choose the level Easy");
+            TabGame.displayTab(0);
         }
 
         else if (userinput.equals(medium)) {
             System.out.println("You choose the level Medium");
+            TabGame.displayTab(1);
         }
 
         else if (userinput.equals(hard)) {
             System.out.println("You choose the level Hard");
+            TabGame.displayTab(2);
         }
-
     }
+
+
 }
