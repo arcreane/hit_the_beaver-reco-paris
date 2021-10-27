0a38944dce688736fe7e50b0ccbd9954ae0e2909
AmalGuedda <guedda94320@gmail.com>
2021-10-27 15:08:42 +0200

    We applie a while loop so that an error message appears for any entry outside of "Easy","Medium"and"Hard"

--- src/com/fass/ChoicePlayer.java
+++ src/com/fass/ChoicePlayer.java
@@ -7,14 +7,18 @@ public class ChoicePlayer {
         String easy ="Easy";
         String medium ="Medium";
         String hard ="Hard";
+        String error ="Error";
 
         Scanner input = new Scanner(System.in);
-        System.out.println("To start the game you had to choose among the following levels :\"Easy\", \"Medium\", \"Hard\" ");
+        System.out.println("To start the game you had to choose among the following levels :\n\"Easy\"\n\"Medium\"\n\"Hard\" ");
         System.out.println("Please choose the level you want");
         String userinput = input.nextLine();
-
+        // We applied a while loop so that an error message appears for any entry outside of "Easy","Medium"and"Hard"
+        while (userinput.equals("Easy")||userinput.equals("Medium")||userinput.equals("Hard")){
+        }
+        System.out.println("Please, write again your choice");
         if (userinput.equals(easy)) {
-            System.out.println("You choose the level Easy");
+                System.out.println("You choose the level Easy");
         }
 
         else if (userinput.equals(medium)) {
@@ -24,6 +28,5 @@ public class ChoicePlayer {
         else if (userinput.equals(hard)) {
             System.out.println("You choose the level Hard");
         }
-
     }
 }
